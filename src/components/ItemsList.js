e174d6a18f909144332ee39b5b91d70376d334e4
Valerie Freeman <valerah7@gmail.com>
2022-10-13 11:37:13 -0500

    wip

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,9 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-export const ItemsList = ({ filteredItems }) => {
+export const ItemsList = () => {
+  const [items, setItems] = useState([]) // returns an array: [stateVariable, setStatefunction] takes one argument: the initial value of the state variable
+
+  // Use Effect watches for state change
+  // It takes two arguments, a function and an array
+  // The array is which states we want to observe
+  // The function is what we want to do when that observed state changes
+  useEffect(() => {
+    console.log('I only run once')
+    fetch(`http://localhost:8088/items`)
+      .then((res) => res.json())
+      .then((itemsArray) => {
+        setItems(itemsArray)
+      })
+  }, []) // An empty dependency array will watch for the initial render of the component and only run the callback on that  initial run.
+
   return (
     <div className="items-container">
-      {filteredItems.map((itemObj) => {
+      {items.map((itemObj) => {
         return (
           <div className="item-card" key={itemObj.id}>
             <img
